3cd27527b2e8c8ef083b5e30ec5f4b7d2d854567
Ruud Janssen <Ruud Janssen>
2017-07-03 22:56:06 +0200

    added Countries ISO codes

--- Import CitiesToCountries.R
+++ Import CitiesToCountries.R
@@ -13,5 +13,12 @@ library(jsonlite)
 
 #original json_file <- "https://github.com/David-Haim/CountriesToCitiesJSON/blob/master/countriesToCities.json"
 json_file <- "Data\\datasets\\countriesToCities.json"
-
 countriesToCities <- fromJSON(json_file, flatten=TRUE)
+
+
+#http://en.wikipedia.org/wiki/ISO_3166-1_alpha-3
+json_file <- "https://comtrade.un.org/data/cache/reporterAreas.json"
+json_file2 <- "https://comtrade.un.org/data/cache/partnerAreas.json"
+
+countriesReporterAreas <- fromJSON(json_file, flatten=TRUE)
+countriesPartnerAreas <- fromJSON(json_file2, flatten=TRUE)
